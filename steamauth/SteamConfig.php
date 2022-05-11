7c6c9756f43fcf6c45fab50948103524d416316b
Matias Næss <Mattimat_lol@hotmail.com>
2022-05-12 00:23:27 +0200

    Update SteamConfig.php

--- steamauth/SteamConfig.php
+++ steamauth/SteamConfig.php
@@ -1,7 +1,7 @@
 <?php
 //Version 3.2
-$steamauth['apikey'] = "4BB9CE6C01BF0610900E7375D1D31F93"; // Your Steam WebAPI-Key found at http://steamcommunity.com/dev/apikey
-$steamauth['domainname'] = "https://steamid64.net/"; // The main URL of your website displayed in the login page
+$steamauth['apikey'] = ""; // Your Steam WebAPI-Key found at http://steamcommunity.com/dev/apikey
+$steamauth['domainname'] = ""; // The main URL of your website displayed in the login page
 $steamauth['logoutpage'] = "../index"; // Page to redirect to after a successfull logout (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
 $steamauth['loginpage'] = "../myprofile"; // Page to redirect to after a successfull login (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
 
